8f42720adfe45d675fd040b83251d1d2c1473bdf
vedantwadekar49@gmail.com <vedantwadekar49@gmail.com>
2026-07-13 13:57:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+ const hamburger = document.getElementById('hamburger');
+        const navLinks = document.getElementById('navLinks');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            navLinks.classList.toggle('open');
+        });
+
+        // Close menu when a link is clicked (optional)
+        document.querySelectorAll('.nav-links a').forEach(link => {
+            link.addEventListener('click', () => {
+                hamburger.classList.remove('active');
+                navLinks.classList.remove('open');
+            });
+        });
